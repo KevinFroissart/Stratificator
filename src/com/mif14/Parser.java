0e7b835bf6403bac1566bca86fb52821d9ab6a40
jeremy <jeremy.gau@etu.univ-lyon1.fr>
2022-05-15 12:36:22 +0200

    Fix not management

--- src/com/mif14/Parser.java
+++ src/com/mif14/Parser.java
@@ -10,47 +10,43 @@ import com.mif14.model.Rule;
 
 public class Parser {
 
-	public static void parse(String filename, Program program) {
-		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				line = handleComment(line);
-				if (isEmpty(line)) {
-					continue;
-				}
-				if (isEDB(line)) {
-					program.addEdb(new Expression(line));
-				}
-				else if (isIDB(line)) {
-					program.addRule(new Rule(line));
-				}
-			}
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void parse(String filename, Program program) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = handleComment(line);
+                if (line.isEmpty()) {
+                    continue;
+                }
+                if (isEDB(line)) {
+                    program.addEdb(new Expression(line));
+                    continue;
+                }
+                if (isIDB(line)) {
+                    program.addRule(new Rule(line));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private static String handleComment(String line) {
-		line = line.trim();
-		if (line.contains("%") && line.charAt(0) != '%') {
-			return line.substring(0, line.indexOf('%')).trim();
-		}
-		return line.contains("%") ? "" : line;
-	}
+    private static String handleComment(String line) {
+        line = line.trim();
+        if (line.contains("%") && line.charAt(0) != '%') {
+            return line.substring(0, line.indexOf('%')).trim();
+        }
+        return line.contains("%") ? "" : line;
+    }
 
-	private static boolean isEmpty(String line) {
-		return line.length() == 0;
-	}
+    private static boolean isEDB(String line) {
+        return line.matches("\\w+\\(\\s*'?\\w+'?\\s*(,\\s*'?\\w+'?\\s*)*\\)\\.");
+    }
 
-	private static boolean isEDB(String line) {
-		return line.matches("\\w+\\(\\s*'?\\w+'?\\s*(,\\s*'?\\w+'?\\s*)*\\)\\.");
-	}
-
-	private static boolean isIDB(String line) {
-		String not = "\\s*(not)?\\s*";
-		String subgoal = "\\w+\\(\\s*'?\\w+'?\\s*(,\\s*'?\\w+'?\\s*)*\\)";
-		String body = not + subgoal + "\\s*(,\\s*" + not + subgoal + ")*\\.";
-		return line.matches(subgoal + "\\s*:-\\s*" + body);
-	}
+    private static boolean isIDB(String line) {
+        String not = "\\s*(not)?\\s*";
+        String subgoal = "\\w+\\(\\s*'?\\w+'?\\s*(,\\s*'?\\w+'?\\s*)*\\)";
+        String body = not + subgoal + "\\s*(,\\s*" + not + subgoal + ")*\\.";
+        return line.matches(subgoal + "\\s*:-\\s*" + body);
+    }
 }
--- src/com/mif14/model/Expression.java
+++ src/com/mif14/model/Expression.java
@@ -6,45 +6,56 @@ import java.util.stream.Collectors;
 
 public class Expression {
 
-	String predicate;
-	List<String> terms;
-	boolean isNegative;
+    String predicate;
+    List<String> terms;
+    boolean isNegative;
 
-	/**
-	 * Constructor with parameters
-	 * @param predicate The predicate's name.
-	 * @param terms The terms of the expression.
-	 */
-	public Expression(String predicate, List<String> terms) {
-		this.isNegative = false;
-		this.predicate = predicate;
-		this.terms = terms;
-	}
+    /**
+     * Constructor with parameters
+     *
+     * @param predicate The predicate's name.
+     * @param terms     The terms of the expression.
+     */
+    public Expression(String predicate, List<String> terms) {
+        this.isNegative = false;
+        this.predicate = predicate;
+        this.terms = terms;
+    }
 
-	public Expression(String expression) {
-		this.isNegative = expression.trim().startsWith("not");
-		String[] split = expression.split("\\(");
-		String predicate = split[0];
-		String termsString = split[1].substring(0, split[1].endsWith(".") ? split[1].length() - 2 : split[1].length() - 1);
-		String[] terms = termsString.replace(" ","").split(",");
-		this.predicate = predicate;
-		this.terms = Arrays.asList(terms);
-	}
+    public Expression(String expression) {
+        expression = expression.trim();
+        this.isNegative = expression.startsWith("not");
+        if (isNegative) expression = removeNot(expression);
+        String[] split = expression.split("\\(");
+        this.predicate = split[0];
+        String termsString = split[1].substring(0, split[1].endsWith(".") ? split[1].length() - 2 : split[1].length() - 1);
+        String[] terms = termsString.replace(" ", "").split(",");
+        this.terms = Arrays.asList(terms);
+    }
 
-	public String getPredicate() {
-		return predicate;
-	}
+    private String removeNot(String expression) {
+        return expression.substring(3).trim();
+    }
 
-	public List<String> getTerms() {
-		return terms;
-	}
+    public String getPredicate() {
+        return predicate;
+    }
 
-	public boolean isNegative() {
-		return this.isNegative;
-	}
+    public List<String> getTerms() {
+        return terms;
+    }
 
-	@Override
-	public String toString() {
-		return predicate + "(" + terms.stream().collect(Collectors.joining(",")) + ")";
-	}
+    public boolean isNegative() {
+        return this.isNegative;
+    }
+
+    @Override
+    public String toString() {
+        return (isNegative ? "not " : "") +
+                predicate + "(" + String.join(", ", terms) + ")";
+    }
+
+    public boolean hasSamePredicate(Expression expression) {
+        return this.predicate.equals(expression.predicate);
+    }
 }
